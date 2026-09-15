f5b03a50b2f8562037384be74e8935e3da6f751e
Vishwjeet Singh Rathore <vishwjeetsingh715@gmail.com>
2026-09-15 22:54:52 +0530

    Time: 0 ms (100%), Space: 54.3 MB (83.33%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.js
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.js
@@ -4,24 +4,35 @@
  * @return {number}
  */
 var maxPalindromes = function(s, k) {
-    let res = 0, n = s.length;
-    if (k === 1) return n;
-
-    const check = (l, r) => {
-        for (; l < r; l++, r--)
-            if (s[l] !== s[r]) return false;
-        return true;
-    };
-
-    for (let i = 0; i <= n - k; i++) {
-        if (check(i, i + k - 1)) {
-            res++;
-            i += k - 1;
-        } else if (i < n - k && check(i, i + k)) {
-            res++;
-            i += k;
+        let ans = 0 
+	
+    const isPalindrome = (str) => {
+        let start = 0
+        let end = str.length - 1
+        while (start < end) {
+            if (str[start] !== str[end]) return false
+            start++
+            end--
         }
+        return true
     }
+    
+    let len = s.length
+    let i = 0
+    
+    while (i + k <= len) {
+        let step = 1
+        if (isPalindrome(s.substring(i, i + k) )) {
+            ans++
+            step = k
+        } else if (i + k + 1 <= len) {
+            if (isPalindrome(s.substring(i, i + k + 1))) {
+                ans++
+                step = k + 1
+            } 
+        }
+        i += step
+    }
+    return ans
 
-    return res;
 };
