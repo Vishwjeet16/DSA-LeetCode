657f0c88bd11ef403b6090e5bacfc4c5c34b9db9
Vishwjeet Singh Rathore <vishwjeetsingh715@gmail.com>
2026-09-21 19:36:35 +0530

    Time: 1213 ms (5.17%), Space: 64 MB (9.86%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let n = s.length;
+    let res = 0;
+    for (let i = 0; i < n; i++) {
+        let charSet = new Set();
+        for (let j = i; j < n; j++) {
+            if (charSet.has(s[j])) {
+                break;
+            } else {
+                charSet.add(s[j]);
+                res = Math.max(res, j - i + 1);
+            }
+        }
+    }
+    return res;
+};
