c287f298f3538fb57d02ade0262bc0e0125d186a
Vishwjeet Singh Rathore <vishwjeetsingh715@gmail.com>
2026-09-23 20:53:21 +0530

    Time: 0 ms (100%), Space: 54 MB (43.41%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -4,17 +4,26 @@
  * @return {number}
  */
 var search = function(nums, target) {
-   let first = 0 , last = nums.length -1
-   while(first<= last){
-    let m = Math.floor((first+last)/2)
-    if(nums[m]===target) return m
-    if(nums[first]<=nums[m]){
-        if(target>=nums[first] && target<=nums[m]) last = m
-        else first = m+1
-    }else{
-        if(target>=nums[m+1] && target<=nums[last]) first = m+1
-        else last = m
+   let left = 0, right = nums.length - 1
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2)
+        if (nums[mid] == target) {
+            return mid
+        }
+        else if (nums[left] <= nums[mid]) {//left half is sorted
+            //now chk if target falls under left to mid, if yes r=mid-1 or else left=mid +1
+            if (target >= nums[left] && target <= nums[mid])
+                right = mid - 1
+            else
+                left = mid + 1
+        }
+        else {  //right half is sorted
+            //now chk if target falls under mid to right,
+            if (target >= nums[mid] && target <= nums[right])
+                left = mid + 1
+            else
+                right = mid - 1
+        }
     }
-   }
-     return -1
+    return -1
 };
